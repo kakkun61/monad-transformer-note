cade5d08b696c1b529bf6884228ff72b7dd12428
Kazuki Okamoto <okamoto-k@klab.com>
2014-01-16 11:45:34 +0900

    moad transform!!! ErrorT!

--- main.hs
+++ main.hs
@@ -25,13 +25,18 @@ runEval = runIdentity . runErrorT
 
 eval :: Env -> Exp -> Eval Value
 eval env (Lit i) = return $ IntVal i
-eval env (Var n) = maybe (fail $ "undefined value: " ++ n) return $ Map.lookup n env
-eval env (Plus e1 e2) = do IntVal i1 <- eval env e1
-                           IntVal i2 <- eval env e2
-                           return $ IntVal (i1 + i2)
+eval env (Var n) = case Map.lookup n env of
+                       Nothing -> throwError $ "undefined value: " ++ n
+                       Just val -> return val
+eval env (Plus e1 e2) = do v1 <- eval env e1
+                           v2 <- eval env e2
+                           case (v1, v2) of
+                               (IntVal i1, IntVal i2) -> return $ IntVal $ i1 + i2
+                               _ -> throwError "type error in plus"
 eval env (Abs n e) = return $ FunVal env n e
 eval env (App e1 e2) = do val1 <- eval env e1
                           val2 <- eval env e2
                           case val1 of
                               FunVal env' n body -> eval (Map.insert n val2 env') body
+                              _ -> throwError "type error in application"
 
