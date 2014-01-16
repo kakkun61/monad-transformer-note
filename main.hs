b4e57754bff0957a0df47cde8713ce9bc4078128
Kazuki Okamoto <okamoto-k@klab.com>
2014-01-16 21:34:52 +0900

    ReaderT monad trandformer

--- main.hs
+++ main.hs
@@ -7,7 +7,7 @@ import Control.Monad.Writer
 import Data.Maybe
 import qualified Data.Map as Map
 
-type Eval a = ErrorT String Identity a
+type Eval a = ReaderT Env (ErrorT String Identity) a
 type Name   = String
 data Exp    = Lit Integer
             | Var Name
@@ -20,23 +20,25 @@ data Value  = IntVal Integer
             deriving (Show)
 type Env    = Map.Map Name Value 
 
-runEval :: Eval a -> Either String a
-runEval = runIdentity . runErrorT
+runEval :: Env -> Eval a -> Either String a
+runEval env ev = runIdentity $ runErrorT $ runReaderT ev env
 
-eval :: Env -> Exp -> Eval Value
-eval env (Lit i) = return $ IntVal i
-eval env (Var n) = case Map.lookup n env of
-                       Nothing -> throwError $ "undefined value: " ++ n
-                       Just val -> return val
-eval env (Plus e1 e2) = do v1 <- eval env e1
-                           v2 <- eval env e2
-                           case (v1, v2) of
-                               (IntVal i1, IntVal i2) -> return $ IntVal $ i1 + i2
-                               _ -> throwError "type error in plus"
-eval env (Abs n e) = return $ FunVal env n e
-eval env (App e1 e2) = do val1 <- eval env e1
-                          val2 <- eval env e2
-                          case val1 of
-                              FunVal env' n body -> eval (Map.insert n val2 env') body
-                              _ -> throwError "type error in application"
+eval :: Exp -> Eval Value
+eval (Lit i) = return $ IntVal i
+eval (Var n) = do env <- ask
+                  case Map.lookup n env of
+                      Nothing -> throwError $ "undefined value: " ++ n
+                      Just val -> return val
+eval (Plus e1 e2) = do v1 <- eval e1
+                       v2 <- eval e2
+                       case (v1, v2) of
+                           (IntVal i1, IntVal i2) -> return $ IntVal $ i1 + i2
+                           _ -> throwError "type error in plus"
+eval (Abs n e) = do env <- ask
+                    return $ FunVal env n e
+eval (App e1 e2) = do val1 <- eval e1
+                      val2 <- eval e2
+                      case val1 of
+                          FunVal env' n body -> local (Map.insert n val2) (eval body)
+                          _ -> throwError "type error in application"
 
