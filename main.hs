878548e6214833ea5208e698a594a8fb4bdff094
Kazuki Okamoto <okamoto-k@klab.com>
2014-02-05 17:40:31 +0900

    StateT! counting evaluation

--- main.hs
+++ main.hs
@@ -7,7 +7,7 @@ import Control.Monad.Writer
 import Data.Maybe
 import qualified Data.Map as Map
 
-type Eval a = ReaderT Env (ErrorT String Identity) a
+type Eval a = ReaderT Env (ErrorT String (StateT Integer Identity)) a
 type Name   = String
 data Exp    = Lit Integer
             | Var Name
@@ -20,25 +20,33 @@ data Value  = IntVal Integer
             deriving (Show)
 type Env    = Map.Map Name Value 
 
-runEval :: Env -> Eval a -> Either String a
-runEval env ev = runIdentity $ runErrorT $ runReaderT ev env
+runEval :: Env -> Integer -> Eval a -> (Either String a, Integer)
+runEval env st ev = runIdentity $ runStateT (runErrorT $ runReaderT ev env) st
 
 eval :: Exp -> Eval Value
-eval (Lit i) = return $ IntVal i
-eval (Var n) = do env <- ask
+eval (Lit i) = do tick
+                  return $ IntVal i
+eval (Var n) = do tick
+                  env <- ask
                   case Map.lookup n env of
                       Nothing -> throwError $ "undefined value: " ++ n
                       Just val -> return val
-eval (Plus e1 e2) = do v1 <- eval e1
+eval (Plus e1 e2) = do tick
+                       v1 <- eval e1
                        v2 <- eval e2
                        case (v1, v2) of
                            (IntVal i1, IntVal i2) -> return $ IntVal $ i1 + i2
                            _ -> throwError "type error in plus"
-eval (Abs n e) = do env <- ask
+eval (Abs n e) = do tick
+                    env <- ask
                     return $ FunVal env n e
-eval (App e1 e2) = do val1 <- eval e1
+eval (App e1 e2) = do tick
+                      val1 <- eval e1
                       val2 <- eval e2
                       case val1 of
                           FunVal env' n body -> local (Map.insert n val2) (eval body)
                           _ -> throwError "type error in application"
 
+tick :: (Num s, MonadState s m) => m ()
+tick = do st <- get
+          put (st + 1)
